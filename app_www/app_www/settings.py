eea32bb92fd3960685a299021f0d98bea8062b60
Kamil Schilling <kamil.schilling@gmail.com>
2021-01-06 15:03:50 +0100

    key

--- app_www/app_www/settings.py
+++ app_www/app_www/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'co!-6774ghby*s=@#s$773(ex=b19r*ua*htx9&w9$0v(-l!4p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
